6e01556f313239bdb195453ca472efbee2c2aec7
Varad Gupta <varadgupta21@gmail.com>
2024-01-16 11:12:12 +0530

    updated send_email

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -6,12 +6,12 @@ st.header('Contact Me')
 with st.form(key='email_form'):
     user_email = st.text_input('Your Email Address')
     raw_message = st.text_area('Your Message')
-    message = f"""\
-    Subject: New mail from {user_email}
-    
+    message = f"""Subject: New mail from {user_email}
+
     From: {user_email}
     {raw_message}
     """
+
     button = st.form_submit_button('Submit')
     if button:
         sendEmail(message)
