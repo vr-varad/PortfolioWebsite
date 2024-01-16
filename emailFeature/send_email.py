007a6aa75c8bf371b293fad6b7cef275a3405537
Varad Gupta <varadgupta21@gmail.com>
2024-01-16 12:10:44 +0530

    made some changes

--- emailFeature/send_email.py
+++ emailFeature/send_email.py
@@ -1,12 +1,13 @@
 import smtplib, ssl
+import streamlit as st
 host = 'smtp.gmail.com'
 port = 465
 
 
 def sendEmail(message):
-    username = 'varadgupta21@gmail.com'
-    password = 'syuisprckzycllji'
-    reciever_email = 'varadgupta21@gmail.com'
+    username = st.secrets['email']
+    password = st.secrets['password']
+    reciever_email = st.secrets['email']
 
     my_context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=my_context) as server:
