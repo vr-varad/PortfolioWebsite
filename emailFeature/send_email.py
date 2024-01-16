6c3588bfb5d459249377ca631a755980cdc23264
Varad Gupta <varadgupta21@gmail.com>
2024-01-16 12:05:45 +0530

    made some changes

--- emailFeature/send_email.py
+++ emailFeature/send_email.py
@@ -1,16 +1,14 @@
-import smtplib,ssl
-
+import smtplib, ssl
 host = 'smtp.gmail.com'
 port = 465
 
 
 def sendEmail(message):
     username = 'varadgupta21@gmail.com'
-    password = 'dqsmcknylnpvzpsy'
+    password = 'syuisprckzycllji'
     reciever_email = 'varadgupta21@gmail.com'
 
     my_context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(host,port,context=my_context ) as server:
-        server.login(username,password)
-        server.sendmail(username,reciever_email,message)
-
+    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.login(username, password)
+        server.sendmail(username, reciever_email, message)
